56c32af6fa23ca796990a6524a3ee5c564f55272
Peter Yang <peteryang1625@gmail.com>
2015-10-10 16:38:45 -0700

    userTempo

--- soundboard.js
+++ soundboard.js
@@ -1,3 +1,7 @@
+var userTempo = document.getElementById("userTempo");
+var tempo = userTempo.getAttribute("value");
+console.log(tempo);
+
 function playAudio(id){
     var audio = document.getElementById(id);
     if(audio.paused){
