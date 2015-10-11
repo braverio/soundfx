b9f85e55d8dfa6ef750f3b51389e194c36794a1d
Peter Yang <peteryang1625@gmail.com>
2015-10-10 17:09:43 -0700

    Playbackrate beta
    It probably doesn't work

--- soundboard.js
+++ soundboard.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
     var userTempo = document.getElementById("userTempo");
-    var tempo = userTempo.getAttribute("value");
-    console.log(tempo);
+    var GlobalTempo = userTempo.getAttribute("value");
+    //Not sure if that works
+    var audioFiles = document.getElementById("continuousMusic").children;
+    //Or this
+    for (audio in audioFiles){
+        setPlayBackRate(audio);
+    }
 });
-
-
 
 function playAudio(id){
     var audio = document.getElementById(id);
@@ -15,4 +18,11 @@ function playAudio(id){
     else {
         audio.pause();
     }
+}
+
+function setPlayBackRate(element){
+    var audio = element;
+    var originalTempo = audio.dataset.tempo;
+    var playbackSpeed = GlobalTempo/originalTempo;
+    audio.playbackRate = playbackSpeed;
 }
