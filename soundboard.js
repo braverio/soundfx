7995357c3ee1f764dd310391d63958d57e92c37a
Brandon <bnguyen9469@yahoo.com>
2016-04-22 14:22:11 -0700

    Keyboard control and new sound files

--- soundboard.js
+++ soundboard.js
@@ -1,16 +1,63 @@
 var audio;
 
+var HIT = 0;
+var RUM = 1;
+var SIR = 2;
+
+function initAudio(){
+    audio = [document.getElementById('hit'),document.getElementById('rumble'),document.getElementById('siren')];
+    
+    audio[HIT].volume = 0.2;
+    audio[SIR].volume = 0.4;
+}
+
 function playAudio(id){
-    audio = document.getElementById(id);
-    if(audio.paused){
-        audio.currentTime = 0;
-        audio.play();
+        
+    var i;
+    
+    switch(id){
+        case 'hit': i = HIT; break;
+        case 'rumble': i = RUM; break;
+        case 'siren': i = SIR; break;
+        default: i = -1;
+    }
+    
+    
+    var currAudio = audio[i];
+    if(currAudio.paused){
+        currAudio.currentTime = 0;
+        currAudio.play();
     }
     else {
-        audio.pause();
+        currAudio.pause();
     }
 }
 
 function stopAudio(){
-    audio.pause();
+    audio.forEach(function(obj, index){
+        if(!obj.paused){
+            obj.pause();
+        }
+    });
+    
 }
+
+$(document).ready(function(){
+    initAudio();
+    
+    $('button').focus(function() {
+        this.blur();
+    });
+    
+    $(document).keyup(function(e){
+        if(e.keyCode == 32){
+            stopAudio();
+        }else if(e.keyCode == 72){//hit
+            playAudio('hit');
+        }else if(e.keyCode == 82){ //rumble
+            playAudio('rumble');
+        }else if(e.keyCode == 83){ //siren
+            playAudio('siren');
+        }
+    });
+});
